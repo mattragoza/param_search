1b2f83abec8127f707e283eb440f7355faaaccf9
mattragoza <mtr22@pitt.edu>
2022-04-13 13:52:42 -0400

    easier queue selection

--- param_search/__init__.py
+++ param_search/__init__.py
@@ -5,6 +5,15 @@ from .params import ParamSpace
 from .results import plot
 
 
+def queue(use='slurm'):
+    if use == 'local':
+        return job_queues.LocalQueue
+    elif use == 'slurm':
+        return job_queues.SlurmQueue
+    elif use == 'torque':
+        return job_queues.TorqueQueue
+
+
 def submit(
     template,
     name_format,
@@ -13,6 +22,7 @@ def submit(
     use='slurm',
     verbose=False,
     merge=True,
+    job_ids=None,
     **kwargs,
 ):
     import pandas as pd
@@ -21,21 +31,16 @@ def submit(
     params = list(param_space)
     job_files = setup(template, name_format, params, work_dir)
 
-    if use == 'local':
-        queue = job_queues.LocalQueue
-    elif use == 'slurm':
-        queue = job_queues.SlurmQueue
-    elif use == 'torque':
-        queue = job_queues.TorqueQueue
-
-    # submit jobs to queue
-    job_ids = queue.submit_job_scripts(job_files, verbose=verbose, **kwargs)
+    if job_ids is None: # submit jobs to queue
+        job_ids = queue(use).submit_job_scripts(
+            job_files, verbose=verbose, **kwargs
+        )
 
     if verbose:
         print(job_ids)
 
     # return job status data frame
-    status = queue.get_job_status(job_ids)
+    status = queue(use).get_job_status(job_ids)
 
     if merge: # combine job params and job status
         params = pd.DataFrame(params)
@@ -45,3 +50,4 @@ def submit(
             return type(status)(params.merge(status, on='job_name'))
     else: # just return status
         return status
+
